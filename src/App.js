76f352174303886d324a4db0d15d10848665fe17
Shumail Steve <shumailsteve@gmail.com>
2021-01-10 03:39:33 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, { useState, useEffect} from 'react';
+import { FormControl, Select, MenuItem } from '@material-ui/core';
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
+
+  // Only runs once when the component loads 
+  useEffect(() => {
+
+    const getCountriesData = async () => {
+      await fetch("http://disease.sh/v3/covid-19/countries")
+        .then((res) => res.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2
+          }));
+
+          setCountries(countries);
+        });
+    };
+
+    getCountriesData();
+  }, []);
+
+  // Update select value 
+  const onCountryChange = (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app-header">
+        <h1>Covid 19 Tracker</h1>
+        <FormControl className="app-dropdown">
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}        
+          
+          </Select>
+        </FormControl>
+      </div>
+       
+      
     </div>
   );
 }
